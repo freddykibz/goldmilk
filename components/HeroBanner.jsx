ca939de3f96442ff4913261f455ac2b760d24d9b
Fred kibuthu <fred@kibzagency.com>
2023-09-14 06:34:37 +0300

    image center

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -13,7 +13,7 @@ const HeroBanner = () => {
 
   return (
     <section id='home'
-      className='flex xl:flex-row flex-col justify-center min-h-screen  max-container px-30 '>
+      className='flex xl:flex-row flex-col flex-center min-h-screen  max-container px-30 '>
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>
         Navigating Dairy's Future
